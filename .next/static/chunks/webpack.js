6ee0e0e8323505f23df1bf01d5f7aec2d74eaa99
dam824 <damiendagory@gmail.com>
2025-03-10 15:35:06 +0100

    update seo meta

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9ee2576fa06eaed5"; }
+/******/ 		__webpack_require__.h = function() { return "e84b5f5e58f491ca"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
